215bdc58b605ea7a59ced8b6490726c2dac1ee3f
Moon ijaz <moonijaz006@gmail.com>
2023-01-02 11:00:30 +0500

    Add details of premitive datatype

---
+++ java-script/premitive-datatype.js
@@ -0,0 +1,11 @@
+// premitive datatypes are of 7 types
+// NN BB SS U
+
+let a = null;
+let b = 345;
+let c = true;
+let d = BigInt("334");
+let e = "hi";
+let f = Symbol("symbol");
+let g = undefined
+console.log (a,b,c,d,e,f,g)
