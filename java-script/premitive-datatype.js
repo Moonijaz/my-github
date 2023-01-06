a78a0b2850ade9b2c9ae602250746cb909eed488
Moon ijaz <moonijaz006@gmail.com>
2023-01-06 11:00:30 +0500

    Add details of premitive datatype

--- java-script/premitive-datatype.js
+++ java-script/premitive-datatype.js
@@ -1,5 +1,6 @@
-// premitive datatypes are of 7 types
+// 1- premitive datatypes are of 7 types
 // NN BB SS U
+// null number BigInt Boolean String Symbol undefined
 
 let a = null;
 let b = 345;
@@ -9,3 +10,5 @@ let e = "hi";
 let f = Symbol("symbol");
 let g = undefined
 console.log (a,b,c,d,e,f,g)
+
+// 2- non-premitive datatype -> objects
